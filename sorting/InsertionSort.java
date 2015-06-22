c144804fcb43b483c073346efdb79a5726d21fda
Cara Borenstein <cara@Caras-MacBook-Pro.local>
2015-06-22 12:19:54 -0400

    Sorting

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort{
+
+    public static void print(String [] names){
+        for(int i = 0; i < names.length; ++i){
+            System.out.println(names[i]);
+        }
+    }
+    public static void main(String [] args){
+        String [] names = new String [] {"Quentin", "Juliana", "Todd", "Max", "Charlie", "Theo" ,"Nathaniel M.", "James", "Kyle", "Matthew", "Nathaniel T.", "David" };
+    
+        /* 
+         *  Enter Your Code Here
+         *
+         */
+        print(names);
+    }
+}
